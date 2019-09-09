620ea46ed2617ef1a176ffaf958f9588c96fd0bd
Chance Smith <chancesmith97@hotmail.com>
2019-09-09 23:06:24 +0000

    Automatically backed up by Learn

---
+++ app/models/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+  attr_accessor :name, :breed, :age 
+  
+  def initialize(name, breed, age) 
+    @name = name 
+    @breed = breed
+    @age = age 
+  end
+  
+  def self.all 
+    Dog.all 
+  end
+  
+  
+  
+end
